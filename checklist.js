52e51f095dfb3203206d7df046904562322bb542
Evgeniy-makdak <makdak23@mail.ru>
2026-01-17 17:58:16 +0300

    Начал реализацию задачи Чеклист

---
+++ checklist.js
@@ -0,0 +1,8 @@
+let input = document.querySelector('#input');
+let list = document.querySelector('#list');
+
+input.addEventListener('keypress', function(event) {
+	if (event.key == 'Enter') {
+		// здесь будет код для добавление новой li в список
+	}
+});
