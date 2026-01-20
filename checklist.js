6361f0e35c614d14ba6c6e204ee149f218430d42
Evgeniy-makdak <makdak23@mail.ru>
2026-01-20 21:47:25 +0300

    Добавил обработчики событий для Удалить и Сделано

--- checklist.js
+++ checklist.js
@@ -1,9 +1,33 @@
-let input = document.querySelector('#input');
-let list = document.querySelector('#list');
+const input = document.querySelector("#input");
+const list = document.querySelector("#list");
 
-input.addEventListener('keypress', function(event) {
-	if (event.key == 'Enter') {
-		list.innerHTML += `<li>${input.value}</li>`;
-		input.value = '';
-	}
+input.addEventListener("keypress", function (event) {
+  if (event.key == "Enter") {
+    const li = document.createElement("li");
+
+    const task = document.createElement("span");
+    task.classList.add("task");
+    task.textContent = this.value;
+    li.appendChild(task);
+
+    const remove = document.createElement("span");
+    remove.classList.add("remove");
+    remove.textContent = "удалить";
+    remove.addEventListener("click", function () {
+      li.remove();
+    });
+    li.appendChild(remove);
+
+    const mark = document.createElement("span");
+    mark.classList.add("mark");
+    mark.textContent = "сделано";
+    li.appendChild(mark);
+    mark.addEventListener("click", function () {
+      li.classList.add("done");
+    });
+
+    list.appendChild(li);
+
+    this.value = "";
+  }
 });
