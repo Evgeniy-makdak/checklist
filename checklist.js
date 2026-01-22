b9ba72d3adfc969f94326a9325f2be61add96740
Evgeniy-makdak <makdak23@mail.ru>
2026-01-22 14:26:50 +0300

    Функционал редактирование

--- checklist.js
+++ checklist.js
@@ -3,37 +3,42 @@ const list = document.querySelector("#list");
 let editingTask = null;
 
 input.addEventListener("keypress", function (event) {
-  console.log(input.value)
   if (event.key == "Enter" && input.value) {
-    const li = document.createElement("li");
+    if (!editingTask) {
+      const li = document.createElement("li");
 
-    const task = document.createElement("span");
-    task.classList.add("task");
-    task.textContent = this.value;
-    task.addEventListener("dblclick", function () {
-      editingTask = task;
-      input.value = task.textContent;
-    });
-    li.appendChild(task);
+      const task = document.createElement("span");
+      task.classList.add("task");
+      task.textContent = this.value;
+      task.addEventListener("dblclick", function () {
+        editingTask = task;
+        input.value = task.textContent;
+      });
+      li.appendChild(task);
 
-    const remove = document.createElement("span");
-    remove.classList.add("remove");
-    remove.textContent = "удалить";
-    remove.addEventListener("click", function () {
-      li.remove();
-    });
-    li.appendChild(remove);
+      const remove = document.createElement("span");
+      remove.classList.add("remove");
+      remove.textContent = "удалить";
+      remove.addEventListener("click", function () {
+        li.remove();
+      });
+      li.appendChild(remove);
 
-    const mark = document.createElement("span");
-    mark.classList.add("mark");
-    mark.textContent = "сделано";
-    li.appendChild(mark);
-    mark.addEventListener("click", function () {
-      li.classList.add("done");
-    });
+      const mark = document.createElement("span");
+      mark.classList.add("mark");
+      mark.textContent = "сделано";
+      li.appendChild(mark);
+      mark.addEventListener("click", function () {
+        li.classList.add("done");
+      });
 
-    list.appendChild(li);
+      list.appendChild(li);
 
-    this.value = "";
+      this.value = "";
+    } else {
+      editingTask.textContent = this.value;
+      this.value = "";
+      editingTask = null;
+    }
   }
 });
